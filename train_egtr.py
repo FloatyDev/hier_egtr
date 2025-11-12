9fbbf820917dcebf672647e6eb9fd7f382a02278
Giorgos Sygkelakis <sygkelakis@ics.forth.gr>
2025-11-12 12:06:49 +0000

    refactor: freeze predictor_gate

--- train_egtr.py
+++ train_egtr.py
@@ -367,7 +367,7 @@ class SGG(pl.LightningModule):
                 # "proj_k",  # key projection
                 # "final_sub_proj",  # keeps sub-object embeddings in sync
                 # "final_obj_proj",  # keeps object embeddings in sync
-                "rel_predictor_gate",  # tiny gate mlp, if you use it
+                #"rel_predictor_gate",  # tiny gate mlp, if you use it
             )
 
             for n, p in self.model.named_parameters():
